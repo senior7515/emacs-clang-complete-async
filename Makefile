3d08cb263f4ac9ad898f3c63ae78f693986ae1ec
agallego <gallego.alexx@gmail.com>
2014-03-02 16:40:24 -0500

    cd /usr/lib/clang-3.4/lib and symlink libclang.so.1 to libclang.so Also add these mods to completion.h and Makefile

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ PROGRAM_NAME    := clang-complete
 LLVM_CONFIG     := llvm-config
 
 LDLIBS := $(shell $(LLVM_CONFIG) --ldflags) -lclang
-CFLAGS += -std=c99 $(shell $(LLVM_CONFIG) --cflags) -Wall -Wextra -pedantic -O3
+CFLAGS += -std=c99 $(shell $(LLVM_CONFIG) --cflags) -Wall -Wextra -pedantic -O3 -I$(shell $(LLVM_CONFIG) --prefix)/include -L$(shell $(LLVM_CONFIG) --prefix)/lib
 
 
 include makefile.mk
--- src/completion.h
+++ src/completion.h
@@ -1,7 +1,7 @@
 #ifndef _COMPLETION_SESSION_H_
 #define _COMPLETION_SESSION_H_
 
-
+#include <stdio.h>
 #include <clang-c/Index.h>
 
 
@@ -43,12 +43,12 @@ typedef struct __completion_Session_struct
 
 
 
-/* Initialize basic information for completion, such as source filename, initial source 
+/* Initialize basic information for completion, such as source filename, initial source
    buffer and command line arguments to pass to clang */
-void 
+void
 __initialize_completionSession(int argc, char *argv[], completion_Session *session);
 
-/* Initialize session object and launch the completion server, preparse the source file and 
+/* Initialize session object and launch the completion server, preparse the source file and
    build the AST for furture code completion requests  */
 void startup_completionSession(int argc, char *argv[], completion_Session *session);
 
